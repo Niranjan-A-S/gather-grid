b7f13d74d1fee24efe5834452f36be6243261325
Niranjan-A-S <niranjan0881@gmail.com>
2024-02-24 03:08:02 +0530

    chore updated next config to turn off strict mode during development

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["utfs.io"]
-    }
+        domains: ['utfs.io']
+    },
+    reactStrictMode: false
 };
 
 export default nextConfig;
