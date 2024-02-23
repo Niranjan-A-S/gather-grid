a36c51ce8b698ffddab41ff76aece62306051680
Niranjan-A-S <niranjan0881@gmail.com>
2024-02-23 14:37:49 +0530

    chore: added copy to clip board functionality to invite modal

--- components/modals/invite-people-modal.tsx
+++ components/modals/invite-people-modal.tsx
@@ -3,14 +3,35 @@ import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { useCopy } from '@/hooks/use-copy';
 import { useModalStore } from '@/hooks/use-modal-store';
-import { Copy, RefreshCw } from 'lucide-react';
-import React, { useMemo } from 'react';
+import { useOrigin } from '@/hooks/use-origin';
+import axios from 'axios';
+import { Check, Copy, RefreshCw } from 'lucide-react';
+import React, { useCallback, useMemo, useState } from 'react';
 
 export const InvitePeopleModal: React.FC = React.memo(() => {
 
-    const { isOpen, onClose, type } = useModalStore();
-    const isModalOpen = useMemo(() => isOpen && type === 'INVITE PEOPLE', [type, isOpen]);
+    const { isOpen, onClose, type, data: { server }, onOpen } = useModalStore();
+    const isModalOpen = useMemo(() => isOpen && type === 'INVITE_PEOPLE', [type, isOpen]);
+
+    const origin = useOrigin();
+    const inviteCode = `${origin}/invite/${server?.inviteCode}`;
+
+    const { isCopied, onCopy } = useCopy(inviteCode);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const onGenerateCode = useCallback(async () => {
+        try {
+            setIsLoading(true);
+            const response = await axios.patch(`api/servers/${server?.id}/invite-code`);
+
+            onOpen('INVITE_PEOPLE', { server: response.data });
+        } catch (error) {
+            setIsLoading(false);
+            console.log(error);
+        }
+    }, [onOpen, server?.id]);
 
     return (
         <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -29,17 +50,24 @@ export const InvitePeopleModal: React.FC = React.memo(() => {
                     <div className="flex items-center mt-2 gap-x-2">
                         <Input
                             className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                            value={'inviteUrl'}
+                            value={inviteCode}
+                            disabled={isLoading}
                         />
-                        <Button size="icon">
-
-                            <Copy className="w-4 h-4" />
+                        <Button size="icon" onClick={onCopy} disabled={isLoading}>
+                            {isCopied
+                                ?
+                                <Check className="w-4 h-4" />
+                                :
+                                <Copy className="w-4 h-4" />
+                            }
                         </Button>
                     </div>
                     <Button
                         variant="link"
                         size="sm"
                         className="text-xs text-zinc-500 mt-4"
+                        disabled={isLoading}
+                        onClick={onGenerateCode}
                     >
                         Generate a new link
                         <RefreshCw className="w-4 h-4 ml-2" />
