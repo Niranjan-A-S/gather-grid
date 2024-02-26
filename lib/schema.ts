762868a4253f2421ba7e298903e0a3217ec9ed8e
Niranjan-A-S <niranjan0881@gmail.com>
2024-02-26 14:48:05 +0530

    chore: updated the create channel form schema

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,3 +1,4 @@
+import { ChannelType } from '@prisma/client';
 import * as z from 'zod';
 
 export const createServerFormSchema = z.object({
@@ -10,7 +11,12 @@ export const createServerFormSchema = z.object({
 });
 
 export const createChannelFormSchema = z.object({
-    name: z.string().min(1, {
-        message: 'Server name is required.'
-    })
+    name: z.string()
+        .min(1, {
+            message: 'Channel name is required.'
+        })
+        .refine(name => name !== 'general', {
+            message: 'Channel name cannot be \'general\''
+        }),
+    type: z.nativeEnum(ChannelType)
 });
