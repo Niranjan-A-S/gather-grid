80d909d8247dc321509fa4f571c74e45e0fe5215
Niranjan-A-S <niranjan0881@gmail.com>
2024-02-27 15:07:15 +0530

    feat: added redirection to general channel functionality

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import { getCurrentProfile } from '@/lib';
+import { db } from '@/lib/db';
+import { redirectToSignIn } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
-const ServerIdPage = () => (
-        <div>Server Id Page</div>
-    );
+const ServerIdPage = async ({ params: { serverId } }: { params: { serverId: string } }) => {
+
+    const profile = await getCurrentProfile();
+    if (!profile) return redirectToSignIn();
+
+    const server = await db.server.findUnique({
+        where: {
+            id: serverId,
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        },
+        include: {
+            channels: {
+                where: {
+                    name: 'general'
+                },
+                orderBy: {
+                    createdAt: 'asc'
+                }
+            }
+        }
+    });
+
+    const initialChannel = server?.channels[0];
+    if (initialChannel?.name !== 'general') return null;
+
+    return redirect(`/servers/${serverId}/channels/${initialChannel?.id}`);
+};
 
 export default ServerIdPage;
