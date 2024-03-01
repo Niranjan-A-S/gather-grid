545e462c9cad37029411346b2d27607ec2e85c4e
Niranjan-A-S <niranjan0881@gmail.com>
2024-03-01 16:40:14 +0530

    chore: organized the imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import { ModalProvider } from '@/components/providers/modal-provider';
+import { QueryProvider } from '@/components/providers/query-provider';
+import { SocketProvider } from '@/components/providers/socket-provider';
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import { cn } from '@/lib/utils';
 import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
 import './globals.css';
-import { SocketProvider } from '@/components/providers/socket-provider';
-import { QueryProvider } from '@/components/providers/query-provider';
 
 const font = Open_Sans({ subsets: ['latin'] });
 
