e326b620082c1e94c26019956d358def1932ca92
Niranjan-A-S <niranjan0881@gmail.com>
2024-03-01 16:39:23 +0530

    chore: updated the auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-    publicRoutes: ['/api/uploadthing'],
-    ignoredRoutes: ['/no-auth-in-this-route']
+    publicRoutes: ['/api/uploadthing']
 });
 
 export const config = {
-    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)']
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)']
 };
