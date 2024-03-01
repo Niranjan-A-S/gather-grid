8f00cf79b4caef65c9ea5262c85d8f7fa0109022
Niranjan-A-S <niranjan0881@gmail.com>
2024-03-01 14:00:50 +0530

    chore: suppressed the error logs by modifying the next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,15 @@ const nextConfig = {
     images: {
         domains: ['utfs.io']
     },
-    reactStrictMode: false
+    reactStrictMode: false,
+    //this webpack config change is for suppressing some errors(buffer util)
+    webpack: (config) => {
+        config.externals.push({
+            'bufferutil': 'commonjs bufferutil',
+            'utf-8-validate': 'commonjs utf-8-validate'
+        });
+        return config;
+    }
 };
 
 export default nextConfig;
