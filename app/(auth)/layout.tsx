f6ad034019e2645fa70c7d19765a5929a66329c0
Niranjan A S <niranjan0881@gmail.com>
2024-07-05 15:38:24 +0530

    chore: added auth checker in auth routes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,11 @@
+import { getCurrentProfile } from '@/lib';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ children }: { children: React.ReactNode }) {
+    const profile = await getCurrentProfile();
+    if (profile) return redirect('/');
+
     return (
         <div className="h-full flex justify-center place-items-center">
             {children}
